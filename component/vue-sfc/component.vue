5967b3826b418be1420f1926b3962f5ecc5c712c
tangxiaomi <81195314@qq.com>
2022-01-27 16:13:27 +0800

    init

---
+++ component/vue-sfc/component.vue
@@ -0,0 +1,23 @@
+<!--
+* ${__className__}
+* @author ${__author__}
+* @date ${__date__}
+* @since ${__version__}
+-->
+
+<template>
+    <div>
+        <h1>${__className__}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ${__className__} extends Vue {
+
+}
+</script>
+
+<style src="./${__component__}.styl" lang="stylus" scoped></style>
