6a941d197e82efbbfe8f1f222e77be958a86c159
tangxiaomi <81195314@qq.com>
2022-01-27 10:36:20 +0800

    vite-project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <img
+            alt="Vue logo"
+            src="./assets/logo.png" />
+        <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/hello'
+
+</script>
+
+<style lang="stylus">
+#app
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+</style>
---
+++ src/components/hello/hello.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        1111
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style src="./hello.styl" lang="stylus" scoped></style>
